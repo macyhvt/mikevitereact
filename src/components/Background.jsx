7ffcb01183aa420870da6956175844d7618c1017
Manoj Kumar <mayankkhatri90@gmail.com>
2024-01-23 11:47:22 +0100

    up

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -5,7 +5,7 @@ function Background() {
     <>
         <div className='fixed z-[2] w-full h-screen'>
             <div className='absolute top-[5%] w-full py-10 flex justify-center text-zinc-300 font-semibold'>@macyhvt</div>
-            <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[14vw] leading-none tracking-tighter text-zinc-300'>DATA KEEP</h1>
+            <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[10vw] leading-none tracking-tighter text-zinc-300'>DATA KEEP</h1>
         </div>
     </>
   )
